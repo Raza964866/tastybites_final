d9016ba42a2eef7b6a6eb6f217ea7d3669c606b8
Syed Raza hussain <122215499+Raza964866@users.noreply.github.com>
2025-07-18 19:42:29 +0500

    config_production.py

--- config_production.py
+++ config_production.py
@@ -5,7 +5,7 @@ class ProductionConfig:
     
     # This will be updated with PythonAnywhere's MySQL details
     # Format: mysql://username:password@username.mysql.pythonanywhere-services.com/username$tastybites_db
-    SQLALCHEMY_DATABASE_URI = 'mysql://raza786:YOUR_DB_PASSWORD@raza786.mysql.pythonanywhere-services.com/raza786$tastybites_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql://raza786:Y_db'
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
@@ -14,6 +14,6 @@ class ProductionConfig:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'syedrazah76@gmail.com'
-    MAIL_PASSWORD = 'dswo gubk ocba qduy'
-    MAIL_DEFAULT_SENDER = 'syedrazah76@gmail.com'
+    MAIL_USERNAME = 'sym'
+    MAIL_PASSWORD = 'dduy'
+    MAIL_DEFAULT_SENDER = 'scom'
