eb2267e23ba44371f42ae1d7c4af7da91f09d3de
Syed Raza hussain <122215499+Raza964866@users.noreply.github.com>
2025-07-18 19:44:34 +0500

    config.py

--- tastybites/backend/config.py
+++ tastybites/backend/config.py
@@ -2,12 +2,12 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_very_secret_key_that_no_one_will_ever_guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:Door%401234@localhost/tastybites_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'syedrazah76@gmail.com'
-    MAIL_PASSWORD = 'dswo gubk ocba qduy'
-    MAIL_DEFAULT_SENDER = 'syedrazah76@gmail.com'
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
+    MAIL_DEFAULT_SENDER = '@gmail.com'
